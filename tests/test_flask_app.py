640b81ad9cd66225f1839aef1536682541e602a7
signebedi <signe@siftingwinnowing.com>
2024-01-21 21:47:51 -0600

    Fixed: test api routes

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -8,17 +8,17 @@ class FlaskAppTestCaseWithoutRateLimiting(unittest.TestCase):
         self.app = app.test_client()
 
     def test_valid_request(self):
-        response = self.app.get('/gita?reference=1&author_id=16')
+        response = self.app.get('/api/gita?reference=1&author_id=16')
         self.assertEqual(response.status_code, 200)
         self.assertIn('content', response.get_json())
 
     def test_invalid_request_no_reference(self):
-        response = self.app.get('/gita?author_id=16')
+        response = self.app.get('/api/gita?author_id=16')
         self.assertEqual(response.status_code, 400)
         self.assertIn('error', response.get_json())
 
     def test_invalid_request_bad_reference(self):
-        response = self.app.get('/gita?reference=invalid&author_id=16')
+        response = self.app.get('/api/gita?reference=invalid&author_id=16')
         self.assertEqual(response.status_code, 400)
         self.assertIn('error', response.get_json())
 
@@ -31,7 +31,7 @@ class FlaskAppTestCaseWithRateLimiting(unittest.TestCase):
     def test_rate_limit(self):
         # Test the rate limit.
         for _ in range(51):  # Assuming the limit is 50 requests per hour
-            response = self.app.get('/gita?reference=1&author_id=16')
+            response = self.app.get('/api/gita?reference=1&author_id=16')
 
         self.assertEqual(response.status_code, 429)
 
