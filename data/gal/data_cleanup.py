34c05456b89e5becf953d7bdc96fa9cf93d3e196
signebedi <signe@siftingwinnowing.com>
2024-02-02 11:43:25 -0600

    Added: working data for rest of Caesar corpus (#35)

--- data/gal/data_cleanup.py
+++ data/gal/data_cleanup.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
 # Load the JSON file into a DataFrame
-df = pd.read_csv('data/gallia/full_corpus_latin_library.csv')
-authors_df = pd.read_json('data/gallia/authors.json')
+df = pd.read_csv('data/gal/full_corpus_latin_library.csv', sep="|")
+authors_df = pd.read_json('data/gal/authors.json')
 authors = list(authors_df[['id', 'name']].itertuples(index=False, name=None))
 authors_dict = dict(authors)
 
 # Function to map author_id to authorName
 def get_author_name(author_id):
     return authors_dict.get(author_id, "Unknown")
+
+# Select the correct commentary
+df = df.loc[df['Commentary'] == "BG"]
 
 # Apply the author-setting function to df (doesn't really apply here)
 df['author_id'] = 16
@@ -26,5 +29,5 @@ df['book_id'] = '2'
 # Drop bloat categories
 df = df[['authorName', 'author_id', 'description', 'verse_number', 'chapter_number', 'full_ref', 'book_id']]
 
-df.to_json('data/gallia/cleaned_data.json')
+df.to_json('data/gal/cleaned_data.json')
 
---
+++ data/hisp/data_cleanup.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+# Load the JSON file into a DataFrame
+df = pd.read_csv('data/hisp/full_corpus_latin_library.csv', sep="|")
+authors_df = pd.read_json('data/hisp/authors.json')
+authors = list(authors_df[['id', 'name']].itertuples(index=False, name=None))
+authors_dict = dict(authors)
+
+# Function to map author_id to authorName
+def get_author_name(author_id):
+    return authors_dict.get(author_id, "Unknown")
+
+# Select the correct commentary
+df = df.loc[df['Commentary'] == "Hisp"]
+
+# Apply the author-setting function to df (doesn't really apply here)
+df['author_id'] = 16
+df['authorName'] = df['author_id'].apply(get_author_name)
+
+# Rename 
+df['verse_number'] = df['Passage']
+df['chapter_number'] = df['Book']
+df['full_ref'] = df['Cite']
+df['description'] = df['Text']
+
+# Add book id
+df['book_id'] = '2'
+
+# Drop bloat categories
+df = df[['authorName', 'author_id', 'description', 'verse_number', 'chapter_number', 'full_ref', 'book_id']]
+
+df.to_json('data/hisp/cleaned_data.json')
+
